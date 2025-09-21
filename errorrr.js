c0badf8481fe1075603ea6238307b6ef757b401c
Adityan <140166085+Adityan-78@users.noreply.github.com>
2025-09-21 13:45:57 +0530

    Update errorrr.js

--- errorrr.js
+++ errorrr.js
@@ -4,4 +4,4 @@ x + = 2; // Error 2: Space in += operator
 console.log(z); // Error 3: Variable z is not defined
 if (x > 5 { // Error 4: Missing closing parenthesis
     alert('Hello' // Error 5: Missing closing parenthesis and semicolon
-}
+} }
